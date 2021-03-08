d861f01a9f34ede2fc12ca403adae396e6f012a0
YJM <jmyoon4488@gmail.com>
2021-03-08 22:47:52 +0900

    [feat] 클래스 및 빈 함수 생성

---
+++ src/main/java/Computer.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Computer {
+    // 기본 숫자
+    private ArrayList<Integer> baseNumbers;
+    // 서로 다른 수로 이루어진 3자리 숫자
+    private ArrayList<Integer> checkNumbers = new ArrayList<>();
+
+    public void generateNumberSet() {
+    }
+
+    public void initNumbers() {
+    }
+
+    public int getRandomIndex() {
+    }
+
+    public int getNumberFromBase(int index) {
+    }
+
+    public ArrayList<Integer> getCheckNumbers() {
+        return checkNumbers;
+    }
+
+    public ArrayList<Integer> getBaseNumbers() {
+        return baseNumbers;
+    }
+
+    public void setBaseNumbers(ArrayList<Integer> baseNumbers) {
+        this.baseNumbers = baseNumbers;
+    }
+
+    public void setCheckNumbers(ArrayList<Integer> checkNumbers) {
+        this.checkNumbers = checkNumbers;
+    }
+}
