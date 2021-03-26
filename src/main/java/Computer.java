981808892b5e8573cd81e30e44f187525ec49a8e
YJM <jmyoon4488@gmail.com>
2021-03-26 23:09:00 +0900

    [refactoring] 리팩토링을 위한 소스 초기화

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,49 +1,7 @@
 import java.util.*;
 
 public class Computer {
-    // 기본 숫자
-    private ArrayList<Integer> baseNumbers;
-    // 서로 다른 수로 이루어진 3자리 숫자
-    private ArrayList<Integer> checkNumbers = new ArrayList<>();
-
-    public void generateNumberSet() {
-        initNumbers();
-        while (checkNumbers.size() < BaseballGame.CHECK_NUMBER_SIZE) {
-            int number = getNumberFromBase(getRandomIndex());
-            if (checkNumbers.contains(number)) continue;
-            checkNumbers.add(number);
-        }
-    }
-
-    public void initNumbers() {
-        baseNumbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-        checkNumbers = new ArrayList<>();
-    }
-
-    public int getRandomIndex() {
-        return (int) (Math.random() * (baseNumbers.size() - 1));
-    }
-
-    public int getNumberFromBase(int index) {
-        if (index >= baseNumbers.size()) return BaseballGame.INPUT_ERROR;
-        int ret = baseNumbers.get(index);
-        baseNumbers.remove(index);
-        return ret;
-    }
-
-    public ArrayList<Integer> getCheckNumbers() {
-        return checkNumbers;
-    }
-
-    public ArrayList<Integer> getBaseNumbers() {
-        return baseNumbers;
-    }
-
-    public void setBaseNumbers(ArrayList<Integer> baseNumbers) {
-        this.baseNumbers = baseNumbers;
-    }
-
-    public void setCheckNumbers(ArrayList<Integer> checkNumbers) {
-        this.checkNumbers = checkNumbers;
+    public String generateRandomNumberStr() {
+        return "";
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,71 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Player {
-    private final String INPUT_MSG = "숫자를 입력해주세요 : ";
-
-    private ArrayList<Integer> numberList;
-    private int strikeCount = 0;
-    private int ballCount = 0;
-
-    Scanner sc = new Scanner(System.in);
-
-    public Player() {}
-
     public int requestInput(boolean isPlaying) {
-        if (isPlaying) System.out.print(INPUT_MSG);
-        String input = sc.next();
-        if (!isPlaying && input.length() == 1 && checkUserInput(input) == 1) return 1;
-        if (!isPlaying) return 2;
-        if (input.length() != BaseballGame.CHECK_NUMBER_SIZE) return BaseballGame.INPUT_ERROR;
-        return checkUserInput(input);
+        return 0;
     }
 
     public int checkUserInput(String txt) {
-        try {
-            return getValidInput(txt);
-        } catch (NumberFormatException e) {
-            return BaseballGame.INPUT_ERROR;
-        }
+        return 0;
     }
 
     public int getValidInput(String text) throws NumberFormatException {
-        if ("".equals(text)) throw new NumberFormatException("Invalid Input");
-        if (text.contains("0")) throw new NumberFormatException("Invalid Input");
-        return Integer.parseInt(text);
-    }
-
-    public ArrayList<Integer> convertNumberToList(int number) {
-        // 서로 다른 수로 이루어진 3자리 숫자
-        ArrayList<Integer> inputNumbers = new ArrayList<>();
-        for (int i = 0; i < BaseballGame.CHECK_NUMBER_SIZE; i++) {
-            inputNumbers.add(0, number % 10);
-            number = number / 10;
-        }
-        return numberList = inputNumbers;
-    }
-
-    public void initBallCount() {
-        strikeCount = 0;
-        ballCount = 0;
-    }
-
-    public ArrayList<Integer> getNumberList() {
-        return numberList;
-    }
-
-    public int getStrikeCount() {
-        return strikeCount;
-    }
-
-    public void setStrikeCount(int strikeCount) {
-        this.strikeCount = strikeCount;
-    }
-
-    public int getBallCount() {
-        return ballCount;
-    }
-
-    public void setBallCount(int ballCount) {
-        this.ballCount = ballCount;
+        return 0;
     }
 }
